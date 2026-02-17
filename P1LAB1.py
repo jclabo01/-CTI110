13e58339444ee26aaec3cd016ec442cadb80bd53
jclabo01 <claboj2782@student.faytechcc.edu>
2026-02-17 10:49:31 -0500

    Add files via upload

---
+++ P1LAB1.py
@@ -0,0 +1,8 @@
+ # Dean Clabo
+ # 2/17/2026
+ # P1LAB1
+ # Learning to use Python
+
+Name = input("Enter your first name: ")
+LastName = input("Enter your last name: ")
+print("Hello, " + Name + " " + LastName +"! " + "Welcome to CTI-110!")
